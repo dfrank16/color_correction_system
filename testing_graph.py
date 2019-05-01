11da898d4d519f9ca7ac99204af7377bb97cf2fb
Dillon Frank <dfrank416@live.com>
2019-05-01 12:16:51 -0400

    Simple system working

--- testing_graph.py
+++ testing_graph.py
@@ -1,57 +1,42 @@
-from __future__ import print_function
-import os
-import sys
-import argparse
-import time
-#import matplotlib.pyplot as matplot
+import matplotlib.pyplot as plt
 import numpy as np
-import easygui as gui
-from PIL import Image, ImageDraw
+import time
 
-
-im = Image.new('RGBA', (300, 300))
-r = 0
-g = 100
-b = 200
-
-
-def main():
-
-    while True:
-        print("\nRGB Raw: ")
-        rgb = (r, g, b)
-        print(rgb_raw)
-
-        for x in range(300):
-            for y in range(300):
-                im.putpixel((x, y), (rgb[0], rgb[1], rgb[2]))
-        im.save('testimage.png')
-        #Image.open('testimage.png').show()
-
-        color = 'testimage.png'
-        msg = 'Color'
-        choices = ["R", "G", "B"]
-        reply = gui.buttonbox(msg, image=color, choices=choices)
-
-
-
-        #color = np.array([rgb_raw[0], rgb_raw[1], rgb_raw[2]])
-        #color = np.expand_dims(color, axis=0)
-
-        #matplot.imshow(color)
-        #matplot.show()
-
-        # Delay for a second and repeat.
-        time.sleep(1.0)
-
-        r = r + 1
-        g = g + 2
-        b = b + 3
-        r = r%256
-        g = g%256
-        b = b%256
-
-
+def update_graph(red, green, blue):
+    red_dec = (red/255)
+    green_dec = (green/255)
+    blue_dec = (blue/255)
+    color_dec = (red_dec, green_dec, blue_dec)
+    graph.fill(x, y, c=color_dec)
 
 if __name__ == '__main__':
-    sys.exit(main())
+    plt.ion()
+    fig, graph = plt.subplots()
+    x = [0, 0, 1, 1]
+    y = [0, 1, 1, 0]
+    
+    r = 0
+    g = 100
+    b = 200
+    
+    while True:
+        update_graph(r, g, b)
+        plt.pause(0.25)
+        plt.draw()
+        
+        r = r + 10
+        g = g + 20
+        b = b + 30
+        r = r%255
+        g = g%255
+        b = b%255
+
+
+
+
+
+
+
+
+
+
